c4d12d67bd9e8f753511760fbba11e6df7a06307
Victor Ung <victor.ung@student.manchester.ac.uk>
2023-10-12 15:45:24 +0100

    Added test account credentials

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,28 +13,44 @@ const Login = () => {
 	};
 
 	return (
-		<form className="login" onSubmit={handleSubmit}>
-			<h3>Login</h3>
+		<>
+			<form className="login" onSubmit={handleSubmit}>
+				<h3>Login</h3>
 
-			<label>Email:</label>
-			<input
-				type="email"
-				onChange={(e) => {
-					setEmail(e.target.value);
-				}}
-				value={email}
-			/>
-			<label>Password:</label>
-			<input
-				type="password"
-				onChange={(e) => {
-					setPassword(e.target.value);
-				}}
-				value={password}
-			/>
-			<button disabled={isLoading}>Login</button>
-			{error && <div className="error">{error}</div>}
-		</form>
+				<label>Email:</label>
+				<input
+					type="email"
+					onChange={(e) => {
+						setEmail(e.target.value);
+					}}
+					value={email}
+				/>
+				<label>Password:</label>
+				<input
+					type="password"
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+					value={password}
+				/>
+				<button disabled={isLoading}>Login</button>
+				{error && <div className="error">{error}</div>}
+			</form>
+
+			<div className="account-credentials">
+				<h3>Test account credentials</h3>
+				<p>
+					Use these credentials if you want to try out the website, and don't
+					want to create an account
+					<br />
+					<p>
+						<strong>Email: </strong>test@gmail.com
+						<br />
+						<strong>Password: </strong>ABCabc123@
+					</p>
+				</p>
+			</div>
+		</>
 	);
 };
 
